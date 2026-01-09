79d0afcbd915d9b91184590789bb95cea8add653
sammythedeveloper <justsammy484@gmail.com>
2026-01-08 22:58:37 -0500

    new plan

--- membership-server/config/stripe.js
+++ membership-server/config/stripe.js
@@ -3,7 +3,7 @@
 export const stripePrices = {
   "Art & Sketch Membership": "price_1SWUYdHyEXHJUIn2bv35yhj7",
   "Book Club Membership": "price_1SWUa6HyEXHJUIn2LP5mOyPc",
-  "Basketball Training Membership": "price_1SWUbsHyEXHJUIn2ahd207ZV",
+  "Challenge Plan": "price_1SWUbsHyEXHJUIn2ahd207ZV",
   "Walking Club Membership": "price_1SWUcYHyEXHJUIn2WBRJGKqw",
   "Free trial": "price_1SnVBxHyEXHJUIn2Dr8E6Jmh",
   "Beginner players plan": "price_1SnVDqHyEXHJUIn24VW4cb9W",
