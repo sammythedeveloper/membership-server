24d050cab32f85ba7b712f73c6b581ee7f96755c
sammythedeveloper <justsammy484@gmail.com>
2026-01-08 21:31:40 -0500

    stipe products

--- membership-server/config/stripe.js
+++ membership-server/config/stripe.js
@@ -5,4 +5,7 @@ export const stripePrices = {
   "Book Club Membership": "price_1SWUa6HyEXHJUIn2LP5mOyPc",
   "Basketball Training Membership": "price_1SWUbsHyEXHJUIn2ahd207ZV",
   "Walking Club Membership": "price_1SWUcYHyEXHJUIn2WBRJGKqw",
+  "Free trial": "price_1SnVBxHyEXHJUIn2Dr8E6Jmh",
+  "Beginner players plan": "price_1SnVDqHyEXHJUIn24VW4cb9W",
+  "Pro Players Plan": "price_1SnVERHyEXHJUIn2k4sI2bXK"
 };
