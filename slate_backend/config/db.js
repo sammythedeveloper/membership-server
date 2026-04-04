2bd6351cfc1e21d1b001070603350fd4f6922f27
sammythedeveloper <justsammy484@gmail.com>
2026-04-04 02:00:00 -0400

    ui of table responsivness on mobile size screen

--- slate_backend/config/db.js
+++ slate_backend/config/db.js
@@ -1,33 +1,33 @@
-import pkg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-const { Pool } = pkg;
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool
-  .connect()
-  .then(() => console.log("✅ Connected to PostgreSQL"))
-  .catch((err) => console.error("❌ Connection error", err));
-export default pool;
-
-
 // import pkg from "pg";
 // import dotenv from "dotenv";
 // dotenv.config();
-
 // const { Pool } = pkg;
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-//   max: 20,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 5000,
-// });
-
-// pool.on("error", (err) => {
-//   console.error("Unexpected error on idle client", err);
-// });
-
+// const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// pool
+//   .connect()
+//   .then(() => console.log("✅ Connected to PostgreSQL"))
+//   .catch((err) => console.error("❌ Connection error", err));
 // export default pool;
+
+
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+});
+
+export default pool;
